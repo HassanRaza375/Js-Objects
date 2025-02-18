5e2590bda29e85eebf1b4ab9068b7f5bf8dd83cc
Syed Hassan Raza <hassanraza@satjapan.co>
2025-02-18 19:02:33 +0500

    updates

--- object.js
+++ object.js
@@ -11,13 +11,14 @@ const library = {
   Objectassign,
   torunForKey,
   getApiData,
+  getFilteredUsers,
   setdataInTable,
   deleteUser,
   editUser,
   createUser,
   recursion,
 };
-
+getApiData()
 function listsBooks() {
   let b = this.books;
   return b;
@@ -81,6 +82,7 @@ async function getApiData() {
     alert("no data found", data);
   }
 }
+
 function setdataInTable() {
   let tablebody = document.getElementById("listing_user_data");
   tablebody.innerHTML = ""; // Clear existing rows
@@ -114,6 +116,45 @@ function setdataInTable() {
 
   tablebody.appendChild(fragment); // Append new rows
 }
+
+function getFilteredUsers($this) {
+  let value = $this?.toLowerCase();
+  let filtered = this.users.filter(
+    (e) => e.name.includes(value) || e.email.includes(value)
+  );
+  let tablebody = document.getElementById("listing_user_data");
+  tablebody.innerHTML = ""; // Clear existing rows
+
+  let fragment = document.createDocumentFragment();
+
+  filtered.forEach((e, i) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `
+            <td>${++i || "N/A"}</td>
+            <td>${e.id || "N/A"}</td>
+            <td>${e.name || "N/A"}</td>
+            <td>${e.email || "N/A"}</td>
+            <td>${e.age || "N/A"}</td>
+            <td>${e.createdAt || "N/A"}</td>
+            <td>${e.updatedAt || "N/A"}</td>
+            <td>
+            <i class="fa fa-trash" onclick="deleteUser(${e.id})"></i>
+            <i class="fa fa-edit" onclick="editUser('${[
+              e.id,
+              e.name,
+              e.email,
+              e.age,
+              e.createdAt,
+              e.updatedAt,
+            ].toString()}')"></i>
+            </td>
+        `;
+    fragment.appendChild(row);
+  });
+
+  tablebody.appendChild(fragment); // Append new rows
+}
+
 async function deleteUser(id) {
   let data = await fetch(`http://localhost:5000/api/users/${id}`, {
     method: "DELETE",
@@ -128,6 +169,7 @@ async function deleteUser(id) {
   alert(data.message);
   this.getApiData();
 }
+
 async function editUser(e, bool, create) {
   let updatedData = await getInputvalues();
   if (create) {
@@ -165,6 +207,7 @@ async function editUser(e, bool, create) {
     }
   }
 }
+
 async function createUser(e) {
   let data = await fetch(`http://localhost:5000/api/users`, {
     method: "POST",
@@ -180,6 +223,7 @@ async function createUser(e) {
   alert(data.id ? "Created" : "Not Created");
   this.getApiData();
 }
+
 function getInputvalues() {
   let id = document.getElementById("ID").value;
   let name = document.getElementById("Name").value;
@@ -190,6 +234,7 @@ function getInputvalues() {
   let updatedData = { id, name, email, age, createdAt, updatedAt };
   return updatedData;
 }
+
 function toggleInputs(mode) {
   // Get the inputs
   const idField = document.getElementById("ID");
@@ -212,6 +257,7 @@ function toggleInputs(mode) {
     document.getElementById("Create").style.display = "inline-block";
   }
 }
+
 function clearInputs() {
   document.getElementById("ID").value = "";
   document.getElementById("Name").value = "";
@@ -220,12 +266,15 @@ function clearInputs() {
   document.getElementById("CreatedAt").value = "";
   document.getElementById("UpdateddAt").value = "";
 }
+
 // library.getApiData();
 function recursion(...arg) {
   console.log(arg);
 }
+
+let data = document.getElementById("card_items");
+let html;
 function SetInterviewQuestions() {
-  let data = document.getElementById("card_items");
   data.innerHTML = `<div id="container">
   <div class="spinner">
     <div></div>
@@ -236,7 +285,7 @@ function SetInterviewQuestions() {
     <div></div>
   </div>
 </div>`;
-  let html = "";
+  html = "";
   console.log(JsonData);
   JsonData.forEach((e, i) => {
     html += `<div class="card">
@@ -249,13 +298,35 @@ function SetInterviewQuestions() {
     data.innerHTML = html;
   }, 2000);
 }
+function filterdata($this) {
+  html = "";
+  let value = $this?.trim("");
+  let filteredata;
+  if (!value) {
+    SetInterviewQuestions();
+  } else {
+    filteredata = JsonData.filter((e) =>
+      e.title.includes(value?.toLowerCase())
+    );
+    filteredata.forEach((e, i) => {
+      html += `<div class="card">
+                <h1>${++i}:${e.question || "none"}</h1>
+                <p>${e.answer || "none"}</p>
+                ${e.code ? `<div class="codeblock">${e.code}</div>` : ""}
+              </div>`;
+    });
+    data.innerHTML = html;
+  }
+}
 let JsonData = [
   {
+    title: "lexical",
     question: "What is Lexical Scope or Lexical Environment",
     answer: `A lexical environment in JavaScript is a data structure that stores variables and functions defined in the current scope
 along with references to all outer scopes. It is also known as the lexical scope.`,
   },
   {
+    title: "var",
     question: `“var” has no block scope`,
     answer: `Variables, declared with var, are either function-scoped or global-scoped. They are visible through blocks.
 For instance:`,
@@ -265,20 +336,34 @@ For instance:`,
 <code class="token function">alert</code><code class="token punctuation">(</code>test<code class="token punctuation">)</code><code class="token punctuation">;</code> <code class="token comment">// true, the variable lives after if</code></em></code>`,
   },
   {
+    title: "closure",
     question: `Closure`,
     answer: `Usually, a function remembers where it was born in the special property [[Environment]]. It references the Lexical Environment from where it’s created (we covered that in the chapter Variable scope, closure).`,
     code: `But when a function is created using new Function, its [[Environment]] is set to reference not the current Lexical Environment, but the global one.`,
   },
   {
+    title: "global object",
     question: `Global object`,
     answer: `The global object provides variables and functions that are available anywhere. By default, those that are built into the language or the environment.In a browser it is named window, for Node.js it is global, for other environments it may have another name.`,
   },
   {
+    title: "function object",
     question: `Function object`,
     answer: `A good way to imagine functions is as callable “action objects”. We can not only call them, but also treat them as objects: add/remove properties, pass by reference etc.`,
     code: `<code class="language-javascript"><code class="token keyword">function</code> <code class="token function">sayHi</code><code class="token punctuation">(</code><code class="token punctuation">)</code> <code class="token punctuation">{</code>
   <code class="token function">alert</code><code class="token punctuation">(</code><code class="token string">"Hi"</code><code class="token punctuation">)</code><code class="token punctuation">;</code>
 <code class="token punctuation">}</code>
 <code class="token function">alert</code><code class="token punctuation">(</code>sayHi<code class="token punctuation">.</code>name<code class="token punctuation">)</code><code class="token punctuation">;</code> <code class="token comment">// sayHi</code></code>`,
+  },
+  {
+    title: "call function",
+    question: `Call function`,
+    answer: `
+    Invokes the function with this explicitly set to thisArg.
+    Passes arguments one by one.
+    call: Arguments are passed individually.
+    The only syntax difference between call and apply is that call expects a list of arguments, while apply takes an array-like object with them.
+    `,
+    code: `function greet(greeting, punctuation) {console.log($'{greeting}, $'{this.name}$'{punctuation}');}const person = { name: "Alice" };greet.call(person, "Hello", "!"); // Output: "Hello, Alice!"`,
   },
 ];
